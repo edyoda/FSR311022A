5110065f5dcb938f5289e702f414dce0b17ee0c9
Divyansh Moonat <DivyanshM@edynamic.net>
2022-12-08 21:39:34 +0530

    Introduction to JS

---
+++ JavaScript/8 Dec/script.js
@@ -0,0 +1,143 @@
+// console.log("Hello from JavaScript");
+// console.log("This is my first JS Program");
+
+
+var x = 500;
+// console.log(x);
+
+
+var y=50;
+// console.log(y);
+
+var z = 1;
+// console.log(z);
+
+// Earphones
+var price = 699;
+// console.log(price);
+
+// var price of earphones = 999; // WRONG (NOT ALLOWED)
+
+var price1 = 999;
+var price2 = 1299;
+var price3 = 499;
+
+
+// var price of earpohnes
+var price_of_earphones = 1000;
+// console.log(price_of_earphones);
+
+var $price = 299;
+// console.log($price);
+
+
+// var 1price = 200; // WRONG (NOT ALLOWED)
+
+var _discount = 100;
+// console.log(_discount);
+
+
+var price = 10;
+var PRICE = 20;
+var Price = 30;
+var priCE = 40;
+var pricE = 50;
+
+// console.log(PRICE);
+
+// var this = 10;
+// var const = 3;
+// var let = 1;
+// var while = 10;
+// var for = 20;
+
+
+// var my product price = 200; // Invalid name
+// var my$product$price = 400; // Valid
+// var my$product price = 500; // Invalid
+// var my$product_price = 200; // Valid
+// var my@product_price = 600; // Invalid
+// var __ = 100; // Valid
+// console.log(__)
+
+// var this = 20; // Invalid
+// var for = 39; // Invalid
+// var thisfor = 22; // Valid
+// var thisthis = 21; // Valid
+// var for_this = 10; // Valid
+// var 1forthis = 11; // Invalid
+
+// Suggest a variable name which will hold the value of the disocunt on earpohnes?
+// EarPhones_discount // Valid
+// discount_earphones // Valid
+// discount_of_earphones // Valid
+// discount price // Invalid
+
+
+// var science_marks = 50;
+// var ScienceMarks = 60;
+var scienceMarks = 70;
+// console.log(scienceMarks);
+
+// var marks = -10;
+// var marks = 15.99;
+// var marks = 70.5;
+// console.log(typeof marks);
+
+var percentage = 25.7;
+// console.log(typeof percentage);
+
+// var userName = "John Doe";
+var userName = 'John Doe';
+// console.log(typeof userName);
+
+
+var a = "10";
+// console.log(typeof a);
+
+var b = 20;
+// console.log(typeof b);
+
+var c = "9806";
+// console.log(typeof c);
+
+
+var d = true;
+// console.log(typeof d);
+
+var e = false;
+// console.log(typeof e);
+
+
+var f = "true";
+// console.log(typeof f);
+
+var g = false;
+// console.log(typeof g);
+
+
+var h = null;
+// console.log(typeof h);
+
+
+var i;
+// console.log(typeof i);
+
+var j = "undefined";
+// console.log(typeof j);
+
+var k = "null";
+// console.log(typeof k);
+
+var l = 'null';
+// console.log(typeof l);
+
+var m = 'undefined';
+// console.log(typeof m);
+
+var n = ""; // Empty string
+// console.log(typeof n);
+
+
+var o = "     "; // String of spaces
+console.log(typeof o);
