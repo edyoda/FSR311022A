05c4e159e5472dc1e49bd61fcec7548ff8e658f4
Divyansh Moonat <DivyanshM@edynamic.net>
2023-01-13 19:41:48 +0530

    Instagram Replica

---
+++ JavaScript/12 Jan/script.js
@@ -0,0 +1,306 @@
+// console.log("Instagram Replica");
+const posts = {
+  meta: {
+    code: 200,
+  },
+  data: [
+    {
+      attribution: null,
+      tags: [],
+      type: "image",
+      location: null,
+      filter: "Normal",
+      created_time: "1440501087",
+      link: "https://instagram.com/p/6zeBG2H1oH/",
+      images: {
+        low_resolution: {
+          url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s320x320/e15/11906267_1671515619746683_1237948463_n.jpg",
+          width: 320,
+          height: 320,
+        },
+        thumbnail: {
+          url: "https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s150x150/e15/11906267_1671515619746683_1237948463_n.jpg",
+          width: 150,
+          height: 150,
+        },
+        standard_resolution: {
+          url: "https://via.placeholder.com/300x650.png/00ff00",
+          width: 640,
+          height: 640,
+        },
+      },
+      users_in_photo: [],
+      caption: {
+        created_time: "1440501087",
+        text: "Logon this is the logo of PRDP, matlab Prem Ratan Dhan Payo. Coming this Diwali",
+        from: {
+          username: "beingsalmankhan",
+          profile_picture:
+            "https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xta1/t51.2885-19/10810066_708040225931789_33645907_a.jpg",
+          id: "1547627005",
+          full_name: "Salman Khan",
+        },
+        id: "1059322357729942165",
+      },
+      user_has_liked: false,
+      id: "1059322354827483655_1547627005",
+      user: {
+        username: "beingsalmankhan",
+        profile_picture:
+          "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+        id: "1547627005",
+        full_name: "Salman Khan",
+      },
+    },
+    {
+      attribution: null,
+      tags: [],
+      type: "image",
+      location: null,
+      filter: "Normal",
+      created_time: "1440499397",
+      link: "https://instagram.com/p/6zay6yBWhr/",
+      images: {
+        low_resolution: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11950690_308991979224936_345640148_n.jpg",
+          width: 320,
+          height: 320,
+        },
+        thumbnail: {
+          url: "https://via.placeholder.com/300x650.png/dc2f56",
+          width: 150,
+          height: 150,
+        },
+        standard_resolution: {
+          url: "https://via.placeholder.com/300x650.png/dc2a56",
+          width: 640,
+          height: 640,
+        },
+      },
+      users_in_photo: [],
+      caption: {
+        created_time: "1440499397",
+        text: "Novitec Rosso\nfollow @Cars @Cars @Cars @Cars | Photographer @cars.of.munich |",
+        from: {
+          username: "carinstagram",
+          profile_picture:
+            "https://igcdn-photos-a-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-19/11375185_1623356661262376_878819060_a.jpg",
+          id: "23275345",
+          full_name: "Kik:SoLeimanRT",
+        },
+        id: "1059308188572412119",
+      },
+      user_has_liked: true,
+      id: "1059308185661565035_23275345",
+      user: {
+        username: "carinstagram",
+        profile_picture:
+          "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+        id: "23275345",
+        full_name: "Kik:SoLeimanRT",
+      },
+    },
+    {
+      attribution: null,
+      tags: ["thehuntingpartytour"],
+      type: "image",
+      location: null,
+      filter: "Normal",
+      created_time: "1440501526",
+      link: "https://instagram.com/p/6ze2yyi4eV/",
+      images: {
+        low_resolution: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11909202_461512274021020_2138194013_n.jpg",
+          width: 320,
+          height: 320,
+        },
+        thumbnail: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11909202_461512274021020_2138194013_n.jpg",
+          width: 150,
+          height: 150,
+        },
+        standard_resolution: {
+          url: "https://via.placeholder.com/300x650.png/dc2aff",
+          width: 640,
+          height: 640,
+        },
+      },
+      users_in_photo: [],
+      caption: {
+        created_time: "1440501526",
+        text: "Before / After - Rybnik tonight. #TheHuntingPartyTour",
+        from: {
+          username: "linkinpark",
+          profile_picture:
+            "https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xfp1/t51.2885-19/11008267_1563640687237549_939346370_a.jpg",
+          id: "4890736",
+          full_name: "LINKIN PARK",
+        },
+        id: "1059326047021663524",
+      },
+      user_has_liked: false,
+      id: "1059326044144371605_4890736",
+      user: {
+        username: "linkinpark",
+        profile_picture:
+          "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+        id: "4890736",
+        full_name: "LINKIN PARK",
+      },
+    },
+    {
+      attribution: null,
+      tags: ["marmaris", "turkey", "dhotel"],
+      type: "image",
+      location: null,
+      filter: "Juno",
+      created_time: "1440495224",
+      link: "https://instagram.com/p/6zS1b4OLoe/",
+      images: {
+        low_resolution: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11875537_123827731297227_268751140_n.jpg",
+          width: 320,
+          height: 320,
+        },
+        thumbnail: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11875537_123827731297227_268751140_n.jpg",
+          width: 150,
+          height: 150,
+        },
+        standard_resolution: {
+          url: "https://via.placeholder.com/300x650.png/dc2a56",
+          width: 640,
+          height: 640,
+        },
+      },
+      users_in_photo: [],
+      caption: {
+        created_time: "1440495224",
+        text: "#marmaris #dhotel #Turkey",
+        from: {
+          username: "sert_mehmet",
+          profile_picture:
+            "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+          id: "7563629",
+          full_name: "Mehmet Se®t",
+        },
+        id: "1059274169221954465",
+      },
+      user_has_liked: false,
+      id: "1059273174265936414_7563629",
+      user: {
+        username: "sert_mehmet",
+        profile_picture:
+          "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+        id: "7563629",
+        full_name: "Mehmet Se®t",
+      },
+    },
+    {
+      attribution: null,
+      tags: [],
+      type: "image",
+      location: null,
+      filter: "Normal",
+      created_time: "1440501996",
+      link: "https://instagram.com/p/6zfwHMEXBY/",
+      images: {
+        low_resolution: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s320x320/e15/11821069_1683591315208464_1921774986_n.jpg",
+          width: 320,
+          height: 320,
+        },
+        thumbnail: {
+          url: "https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11821069_1683591315208464_1921774986_n.jpg",
+          width: 150,
+          height: 150,
+        },
+        standard_resolution: {
+          url: "https://via.placeholder.com/300x650.png/892f56",
+          width: 640,
+          height: 640,
+        },
+      },
+      users_in_photo: [],
+      caption: {
+        created_time: "1440501996",
+        text: "jiff💙",
+        from: {
+          username: "jiffpom",
+          profile_picture:
+            "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+          id: "194146115",
+          full_name: "jiff",
+        },
+        id: "1059329986589192786",
+      },
+      user_has_liked: true,
+      id: "1059329983057588312_194146115",
+      user: {
+        username: "jiffpom",
+        profile_picture:
+          "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+        id: "194146115",
+        full_name: "jiff",
+      },
+    },
+  ],
+};
+
+function createPostCard(post) {
+  // Create and return the card
+  const card = document.createElement("div");
+  card.classList.add("feeds-card");
+
+  card.innerHTML = `
+  <div class="header">
+  <div>
+    <img src="${post.user.profile_picture}" alt="User Profile Picture" />
+    <span>${post.user.username}</span>
+  </div>
+  <div>
+    <i class="fa-solid fa-ellipsis"></i>
+  </div>
+</div>
+<img class="post-img" src="${post.images.standard_resolution.url}" />
+<div class="cta">
+  <div>
+    <i class="fa-regular fa-heart ${
+      post.user_has_liked === true ? "fa-solid red" : ""
+    }"></i>
+    <i class="fa-regular fa-comment"></i>
+    <i class="fa-regular fa-paper-plane"></i>
+  </div>
+  <div>
+    <i class="fa-regular fa-bookmark"></i>
+  </div>
+</div>
+  `;
+
+  return card;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 1. Get the feeds JSON
+  // 2. Run the loop on it and create card with data
+  const feedsSection = document.getElementById("feedsSection");
+  const postsList = posts.data;
+  for (let i = 0; i < postsList.length; i++) {
+    const post = postsList[i];
+    const card = createPostCard(post);
+    feedsSection.append(card);
+  }
+  console.log(postsList);
+
+  // HEADER DATA
+  // post.user.profile_picture => Profile pic of the user
+  // post.user.username => User name of the user
+
+  // Post Image Data
+  // post.images.standard_resolution.url => Src of the post image
+
+  // Foofter data
+  //   post.user_has_liked => Boolean value to check if the user has liked the post
+
+  // 3. In the loop append the data to the feeds section
+});
--- JavaScript/project/product-details.js
+++ JavaScript/project/product-details.js
@@ -5,7 +5,15 @@ const params = new URLSearchParams(window.location.search);
 
 const pId = params.get("productId");
 
-
 // $.get("https://5d76bf96515d1a0014085cf9.mockapi.io/product/" + pId)
 //   .done()
 //   .fail();
+const cartItem = [
+  {
+    // item object,
+    quantity: 2,
+  },
+];
+localStorage.setItem("cart", cartItem);
+
+localStorage.getItem("cart");
